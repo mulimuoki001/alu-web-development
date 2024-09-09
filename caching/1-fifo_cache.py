ae6e8c0df72dfe65bd24990dbaeef5989c4ccd06
mulimuoki001 <b.muoki@alustudent.com>
2024-09-10 00:52:37 +0300

    fifo cache

---
+++ caching/1-fifo_cache.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""FIFOCache class"""
+import collections
+from base_caching import BaseCaching
+
+"""
+FIFOCache defines:
+    - constants of your caching system
+    - where your data are stored (in a dictionary)
+"""
+
+
+class FIFOCache(BaseCaching):
+    """FIFOCache defines:
+    - constants of your caching system
+    - where your data are stored (in a dictionary)
+    """
+
+    def __init__(self):
+        """Initiliaze"""
+        super().__init__()
+        self.cache_data = collections.OrderedDict()
+
+    def put(self, key, item):
+        """Add an item in the cache"""
+        if key is None or item is None:
+            return
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            if key in self.cache_data:
+                self.cache_data.pop(key)
+            else:
+                print("DISCARD: {}".format(self.cache_data.popitem(last=False)[0]))
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Get an item by key"""
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data.get(key)
