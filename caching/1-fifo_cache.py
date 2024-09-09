cac11c7ea131edd83eb30b9bc13e21d758c4c471
mulimuoki001 <b.muoki@alustudent.com>
2024-09-10 01:16:27 +0300

    fifo cache

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -29,8 +29,8 @@ class FIFOCache(BaseCaching):
             if key in self.cache_data:
                 self.cache_data.pop(key)
             else:
-                item = self.cache_data.popitem(last=False)
-                print("DISCARD: {}".format(item[0]))
+                items = self.cache_data.popitem(last=False)
+                print("DISCARD: {}".format(items[0]))
         self.cache_data[key] = item
 
     def get(self, key):
@@ -38,3 +38,17 @@ class FIFOCache(BaseCaching):
         if key is None or key not in self.cache_data:
             return None
         return self.cache_data.get(key)
+
+
+my_cache = FIFOCache()
+my_cache.put("A", "Hello")
+my_cache.put("B", "World")
+my_cache.put("C", "Holberton")
+my_cache.put("D", "School")
+my_cache.print_cache()
+my_cache.put("E", "Battery")
+my_cache.print_cache()
+my_cache.put("C", "Street")
+my_cache.print_cache()
+my_cache.put("F", "Mission")
+my_cache.print_cache()
