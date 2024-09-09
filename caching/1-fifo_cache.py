6ffa7fa8c4c0cc27ba4024ceaec201a469734ff5
mulimuoki001 <b.muoki@alustudent.com>
2024-09-10 01:17:56 +0300

    fifo cache

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -38,17 +38,3 @@ class FIFOCache(BaseCaching):
         if key is None or key not in self.cache_data:
             return None
         return self.cache_data.get(key)
-
-
-my_cache = FIFOCache()
-my_cache.put("A", "Hello")
-my_cache.put("B", "World")
-my_cache.put("C", "Holberton")
-my_cache.put("D", "School")
-my_cache.print_cache()
-my_cache.put("E", "Battery")
-my_cache.print_cache()
-my_cache.put("C", "Street")
-my_cache.print_cache()
-my_cache.put("F", "Mission")
-my_cache.print_cache()
