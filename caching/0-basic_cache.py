e8a5d501ceadf08af1de125555d8ccf3765cd327
mulimuoki001 <b.muoki@alustudent.com>
2024-09-10 00:44:07 +0300

    basic cache

---
+++ caching/0-basic_cache.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+
+import importlib
+
+BaseCaching = importlib.import_module("base_caching").BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """BasicCache defines:
+    - constants of your caching system
+    - where your data are stored (in a dictionary)
+    """
+
+    def put(self, key, item):
+        """Add an item in the cache"""
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Get an item by key"""
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data.get(key)
