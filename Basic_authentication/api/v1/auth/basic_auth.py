6acbcc8dd8149808515329a023a1840ee5b30d23
mulimuoki001 <b.muoki@alustudent.com>
2024-09-16 21:55:03 +0300

    Basic - Base64 part

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,4 +7,12 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """Basic Auth class"""
 
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """Extract base64"""
+        if authorization_header is None:
+            return None
+        if type(authorization_header) != str:
+            return None
+        if authorization_header[:6] != "Basic ":
+            return None
+        return authorization_header[6:]
