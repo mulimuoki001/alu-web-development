5da6c43c3278ab84ca3b4bf602d5fc73fff4ff82
mulimuoki001 <b.muoki@alustudent.com>
2024-09-16 22:31:38 +0300

    Basic - Overload current_user - and BOOM!

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,13 +4,13 @@
 from api.v1.auth.auth import Auth
 import base64
 from typing import TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
     """Basic Auth class"""
 
-    def extract_base64_authorization_header(self,
-                                            authorization_header: str) -> str:
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
         """Extract base64"""
         if authorization_header is None:
             return None
@@ -29,8 +29,7 @@ class BasicAuth(Auth):
         if type(base64_authorization_header) != str:
             return None
         try:
-            return base64.b64decode(
-                base64_authorization_header).decode("utf-8")
+            return base64.b64decode(base64_authorization_header).decode("utf-8")
         except Exception:
             return None
 
@@ -67,3 +66,11 @@ class BasicAuth(Auth):
             return None
         except Exception:
             return None
+
+    def current_user(self, request=None) -> TypeVar("User"):
+        """Current user"""
+        header = self.authorization_header(request)
+        base64_auth = self.extract_base64_authorization_header(header)
+        decoded_base64_auth = self.decode_base64_authorization_header(base64_auth)
+        user_email, user_pwd = self.extract_user_credentials(decoded_base64_auth)
+        return self.user_object_from_credentials(user_email, user_pwd)
