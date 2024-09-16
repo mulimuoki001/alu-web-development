efb46af22e0f7184661c3761a53741ed1e7f62ec
mulimuoki001 <b.muoki@alustudent.com>
2024-09-16 22:20:17 +0300

    Basic - User credentials

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -30,3 +30,16 @@ class BasicAuth(Auth):
             return base64.b64decode(base64_authorization_header).decode("utf-8")
         except Exception:
             return None
+
+    def extract_user_credentials(
+        self, decoded_base64_authorization_header: str
+    ) -> (str, str):
+        """Extract user credentials"""
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if type(decoded_base64_authorization_header) != str:
+            return None, None
+        if ":" not in decoded_base64_authorization_header:
+            return None, None
+        user_pass = decoded_base64_authorization_header.split(":", 1)
+        return user_pass[0], user_pass[1]
