d9b3c898e1060e31a55c1ba5b7af022e62937d27
mulimuoki001 <b.muoki@alustudent.com>
2024-09-16 19:36:24 +0300

    changes

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -3,14 +3,11 @@
 Route module for the API
 """
 from os import getenv
-
-from flask import Blueprint
-
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+from api.v1.views import app_views
+from flask import Flask, jsonify, abort, request
+from flask_cors import CORS, cross_origin
 import os
 
-from flask import Flask, abort, jsonify, request
-from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
