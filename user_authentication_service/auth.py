48c8f7ce46305ee95dd98d15878de0a4a7c8c30d
mulimuoki001 <b.muoki@alustudent.com>
2024-09-26 06:56:12 +0300

    create user and return valuerror if user exists

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -3,6 +3,31 @@
 
 """Hashed password management for the 'users' table."""
 from bcrypt import hashpw, gensalt
+from sqlalchemy.orm.exc import NoResultFound
+
+from db import DB
+
+
+class Auth:
+    """Auth class to interact with the authentication database."""
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> None:
+        """
+        Registers a new user in the database.
+
+        Args:
+            email (str): User's email address.
+            password (str): Password for the user.
+        """
+
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError("User {} already exists".format(email))
+        except NoResultFound:
+            self._db.add_user(email, _hash_password(password))
 
 
 def _hash_password(password: str) -> str:
