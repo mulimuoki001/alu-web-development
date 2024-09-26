f3df12edefbcdf3a22c226b6bdf423d037a88e0e
mulimuoki001 <b.muoki@alustudent.com>
2024-09-26 05:56:06 +0300

    add_user method

---
+++ user_authentication_service/db.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+
+"""SQLAlchemy model for the 'users' table."""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from user import Base
+
+
+class DB:
+    """
+    Database class that handles database connections and operations.
+
+    Attributes:
+        _engine (Engine): SQLAlchemy engine object.
+        __session (Session): SQLAlchemy session object.
+    """
+
+    def __init__(self):
+        """
+        Initializes the database engine and creates/drops tables.
+
+        Creates a SQLite database engine with the `a.db`
+        file and sets `echo=True` to enable database logging.
+        Drops all existing tables and creates new ones
+        based on the `Base` metadata.
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self):
+        """
+        Gets the current database session.
+
+        If no session exists, creates a new one using the
+        `sessionmaker` function and binds it to the database engine.
+
+        Returns:
+            Session: SQLAlchemy session object.
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> None:
+        """
+        Adds a new user to the database.
+
+        Args:
+            email (str): User's email address.
+            hashed_password (str): Hashed password for the user.
+
+        Returns:
+            None
+        """
+        from user import User
+
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
