30e49c259e309de96c9c65818fafed4ff597fa46
mulimuoki001 <b.muoki@alustudent.com>
2024-09-26 05:46:31 +0300

    user model

---
+++ user_authentication_service/user.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+
+"""SQLAlchemy model for the 'users' table."""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+    """
+    SQLAlchemy model for the 'users' table.
+
+    Attributes:
+        id (int): Unique identifier for the user.
+        email (str): User's email address.
+        hashed_password (str): Hashed password for the user.
+        session_id (str): Session ID for the user (optional).
+        reset_token (str): Password reset token for the user (optional).
+    """
+
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    session_id = Column(String, nullable=True)
+    reset_token = Column(String, nullable=True)
