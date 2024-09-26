17b094a80766edf5dabcaa3bf2a2b1dffa1d99a0
mulimuoki001 <b.muoki@alustudent.com>
2024-09-27 02:08:48 +0300

    destroy user

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -61,6 +61,7 @@ def logout() -> str:
     user = AUTH.get_user_from_session_id(session_id)
     if user:
         AUTH.destroy_session(user.id)
+        return redirect("/")
     else:
         abort(403)
 
