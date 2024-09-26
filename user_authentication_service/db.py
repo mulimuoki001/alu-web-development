75caf937ef3c17c3350de99af0e91651ff02bf27
mulimuoki001 <b.muoki@alustudent.com>
2024-09-26 06:15:51 +0300

    add_user method

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -65,12 +65,3 @@ class DB:
         self._session.add(user)
         self._session.commit()
         return user
-
-
-my_db = DB()
-
-user_1 = my_db.add_user("test@test.com", "SuperHashedPwd")
-print(user_1.id)
-
-user_2 = my_db.add_user("test1@test.com", "SuperHashedPwd1")
-print(user_2.id)
